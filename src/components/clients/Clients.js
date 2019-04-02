3fc97a22083f077ef64f18c4528e2cdecad4e489
borulchenko <ruslan.borulchenko@gmail.com>
2019-04-02 23:53:42 +0300

    implemented firebase/firestore integrations

--- src/components/clients/Clients.js
+++ src/components/clients/Clients.js
@@ -55,7 +55,7 @@ class Clients extends Component {
                       to={`/clients/${client.id}`}
                       className="btn btn-secondary btn-sm"
                     >
-                      <i className="fas fa-arrow-circle-right">Details</i>
+                      <i className="fas fa-arrow-circle-right" /> Details
                     </Link>
                   </td>
                 </tr>
